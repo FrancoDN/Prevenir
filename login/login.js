bf528421b81472bf2f0ebe5b655d29ca7e52e893
Franco <dinittofranco@gmail.com>
2020-06-22 04:53:06 -0300

    Visual de login terminada

--- login/login.js
+++ login/login.js
@@ -18,17 +18,36 @@ var ref = firebase.database().ref('Personas');
 
 
 //Login
-const loginFormu = document.querySelector('#loginform');
+const loginFormu = document.getElementById('loginform');
+
+
+//Auth
 loginFormu.addEventListener('submit', (e) => {
     e.preventDefault();
+    console.log("apretadoide")
 
     const email = loginFormu['email'].value;
     const dni = loginFormu['dni'].value;
 
     firebase.auth().signInWithEmailAndPassword(email, getId('dni')).then(cred => {
         console.log(cred);
-    })
-})
+    }).catch(function(error) {
+
+    
+        var errorCode = error.code;
+    
+        if (errorCode == 'auth/wrong-password' || email.value == null || dni.value == null) {
+            alert('Email o contraseña erroneos.');
+          } else if (errorCode != 'auth/wrong-password' && email.value != null && dni.value != null){
+            alert('Usuario indentificado!');
+          }
+    
+        loginFormu.reset();
+        
+      });
+});
+
+//TODO: Limpiar codigo -------------------
 
 /*
 
