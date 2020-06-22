b1cede8d01dc75bcac4691c0d2b53590bfc99e75
castellaniemiliano <castellani.elc@gmail.com>
2020-06-22 14:32:26 -0300

    Login, botonera con current user funcando

---
+++ botonera/botonera.js
@@ -0,0 +1,38 @@
+// Persistencia de usuario
+
+var IDLE_TIMEOUT = 10 * 60; // 10 minutes of inactivity
+var _idleSecondsCounter = 0;
+document.onclick = function() {
+    _idleSecondsCounter = 0;
+};
+document.onmousemove = function() {
+    _idleSecondsCounter = 0;
+};
+document.onkeypress = function() {
+    _idleSecondsCounter = 0;
+};
+window.setInterval(CheckIdleTime, 1000);
+
+function CheckIdleTime() {
+    _idleSecondsCounter++;
+    //console.log(_idleSecondsCounter);
+    if (_idleSecondsCounter >= IDLE_TIMEOUT) {
+        // Cierra la sesion de firebase 
+        firebase.auth().signOut().then(function() {
+            alert("Sesión cerrada por inactividad");
+            window.open('file:///C:/workspace/Prevenir-develop/login/login.html', "_self");
+        }).catch(function(error) {
+            alert("Error");
+        });
+    }
+}
+// Autentificacion al ingresar
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        console.log('Estás logeado.');
+    } else {
+        alert("Debe autentificarse antes de acceder a esta página");
+        window.open('file:///C:/workspace/Prevenir-develop/Baja_prevenir/public/login.html', "_self");
+    }
+});
--- login/login.js
+++ login/login.js
@@ -1,22 +1,3 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyDDRyqvjiteCRv9dtsvepBFntWCj2_2u7A",
-    authDomain: "coronapp-b1f3c.firebaseapp.com",
-    databaseURL: "https://coronapp-b1f3c.firebaseio.com",
-    projectId: "coronapp-b1f3c",
-    storageBucket: "coronapp-b1f3c.appspot.com",
-    messagingSenderId: "971835283337",
-    appId: "1:971835283337:web:0637a51c09501d1970b358",
-    measurementId: "G-HM80TBP5GQ"
-};
-firebase.initializeApp(firebaseConfig);
-
-var ref = firebase.database().ref('Personas');
-
-
-//Constantes
-
-
-
 //Login
 const loginFormu = document.getElementById('loginform');
 
@@ -33,18 +14,18 @@ loginFormu.addEventListener('submit', (e) => {
         console.log(cred);
     }).catch(function(error) {
 
-    
+
         var errorCode = error.code;
-    
+
         if (errorCode == 'auth/wrong-password' || email.value == null || dni.value == null) {
             alert('Email o contraseña erroneos.');
-          } else if (errorCode != 'auth/wrong-password' && email.value != null && dni.value != null){
+        } else if (errorCode != 'auth/wrong-password' && email.value != null && dni.value != null) {
             alert('Usuario indentificado!');
-          }
-    
+        }
+
         loginFormu.reset();
-        
-      });
+
+    });
 });
 
 //TODO: Limpiar codigo -------------------
