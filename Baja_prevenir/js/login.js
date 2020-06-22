91d985b7034f02e827937f226d030ac582a12a59
Franco <dinittofranco@gmail.com>
2020-06-22 12:21:07 -0300

    Baja registros

---
+++ Baja_prevenir/js/login.js
@@ -0,0 +1,46 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDDRyqvjiteCRv9dtsvepBFntWCj2_2u7A",
+    authDomain: "coronapp-b1f3c.firebaseapp.com",
+    databaseURL: "https://coronapp-b1f3c.firebaseio.com",
+    projectId: "coronapp-b1f3c",
+    storageBucket: "coronapp-b1f3c.appspot.com",
+    messagingSenderId: "971835283337",
+    appId: "1:971835283337:web:0637a51c09501d1970b358",
+    measurementId: "G-HM80TBP5GQ"
+  };
+  // Inicializamos Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+// Ingreso
+const signInForm = document.getElementById("login");
+var contador = 0;
+signInForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = signInForm["login-email"].value;
+  const password = signInForm["login-password"].value;
+
+  // Autenticamos al usuario
+
+  firebase.auth().signInWithEmailAndPassword(email, password).then(userCredential => {
+      alert('Ingreso exitoso!');
+      signInForm.reset();
+      console.log("ingreso exitoso");
+      window.open('http://192.168.0.110:4000/public/search.html', "_self");
+ 
+  
+  }).catch(function(error) {
+
+    
+    var errorCode = error.code;
+
+    if (errorCode == 'auth/wrong-password' || email.value == null || password.value == null) {
+        alert('Email o contraseña erroneos.');
+      } else if (errorCode != 'auth/wrong-password' && email.value != null && password.value != null){
+        alert('Usuario indentificado!');
+      }
+
+    signInForm.reset();
+    
+  });
+});
---
+++ Baja_prevenir/js/search.js
@@ -0,0 +1,291 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDDRyqvjiteCRv9dtsvepBFntWCj2_2u7A",
+    authDomain: "coronapp-b1f3c.firebaseapp.com",
+    databaseURL: "https://coronapp-b1f3c.firebaseio.com",
+    projectId: "coronapp-b1f3c",
+    storageBucket: "coronapp-b1f3c.appspot.com",
+    messagingSenderId: "971835283337",
+    appId: "1:971835283337:web:0637a51c09501d1970b358",
+    measurementId: "G-HM80TBP5GQ"
+  };
+  // Inicializamos Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+var rootRef = database.ref('Personas')
+
+rootRef.orderByChild('Caso_sospechoso').equalTo('Si').once('value', function(snapshot) {  
+    var a = snapshot.numChildren();
+    
+    rootRef.orderByChild('Caso_sospechoso').equalTo('No').once('value', function(snapshot) {  
+
+        var numCasos = document.getElementById('casos');
+        numCasos.innerHTML ='';  
+        var b = snapshot.numChildren();
+    
+        numCasos.innerHTML += `
+        <tr>
+        <td>${a}</td>
+        <td>${b}</td>
+        </tr>
+        `
+    
+    });
+});
+
+
+
+signOut.addEventListener('click', (e) => {
+    e.preventDefault();
+    firebase.auth().signOut().then(function() {
+        alert("Sesión cerrada");
+        window.open('http://192.168.0.110:4000/public/login.html', "_self");
+      }).catch(function(error) {
+        alert("No se a podido cerrar la sesión, intente nuevamente");
+      });
+    });
+
+    // Persistencia de usuario
+
+    var IDLE_TIMEOUT = 10 * 60;  // 10 minutes of inactivity
+    var _idleSecondsCounter = 0;
+    document.onclick = function() {
+        _idleSecondsCounter = 0;
+    };
+    document.onmousemove = function() {
+        _idleSecondsCounter = 0;
+    };
+    document.onkeypress = function() {
+        _idleSecondsCounter = 0;
+    };
+    window.setInterval(CheckIdleTime, 1000);
+    function CheckIdleTime() {
+        _idleSecondsCounter++;
+        //console.log(_idleSecondsCounter);
+        if (_idleSecondsCounter >= IDLE_TIMEOUT) {
+            // Cierra la sesion de firebase 
+            firebase.auth().signOut().then(function() {
+                alert("Sesión cerrada por inactividad");
+                window.open('http://192.168.0.110:4000/public/login.html', "_self");
+              }).catch(function(error) {
+                alert("Error");
+              });
+        }
+    }
+
+// Autentificacion al ingresar
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      
+   
+
+// Datos completos
+
+var tabla = document.getElementById('tabla');
+
+rootRef.on('value', function(snapshot) {
+    tabla.innerHTML ='';   
+snapshot.forEach(function(childSnapshot){
+    var childKey = childSnapshot.key;
+    var childData = childSnapshot.val();
+
+     
+    tabla.innerHTML += `
+    <tr>
+    <th scope="row">${childData.dni}</th>
+    <td>${childData.nombre}</td>
+    <td>${childData.apellido}</td>
+    <td>${childData.email}</td>  
+    <td>${childData.telefono}</td>
+    <td>${childData.edad}</td>
+    <td>${childData.direc}</td>
+    <td>${childData.sexo}</td>
+    <td>${childData.Personal_esencial}</td>
+    <td>${childData.Perdida_gusto_olfato}</td>
+    <td>${childData.Fiebre}</td>
+    <td>${childData.Dolor_de_garganta}</td>
+    <td>${childData.Contacto_estrecho}</td>
+    <td>${childData.Dificultad_respiratoria}</td>
+    <td>${childData.Caso_sospechoso}</td>
+    </tr>
+    `
+
+});
+    
+});
+
+// Filtro por personal esencial
+
+esencial.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    var tabla = document.getElementById('tabla');
+
+rootRef.orderByChild('Personal_esencial').equalTo('Si').on('value', function(snapshot) {
+    tabla.innerHTML ='';   
+snapshot.forEach(function(childSnapshot){
+    var childKey = childSnapshot.key;
+    var childData = childSnapshot.val();
+
+     
+    tabla.innerHTML += `
+    <tr>
+    <th scope="row">${childData.dni}</th>
+    <td>${childData.nombre}</td>
+    <td>${childData.apellido}</td>
+    <td>${childData.email}</td>  
+    <td>${childData.telefono}</td>
+    <td>${childData.edad}</td>
+    <td>${childData.direc}</td>
+    <td>${childData.sexo}</td>
+    <td>${childData.Personal_esencial}</td>
+    <td>${childData.Perdida_gusto_olfato}</td>
+    <td>${childData.Fiebre}</td>
+    <td>${childData.Dolor_de_garganta}</td>
+    <td>${childData.Contacto_estrecho}</td>
+    <td>${childData.Dificultad_respiratoria}</td>
+    <td>${childData.Caso_sospechoso}</td>
+    </tr>
+    `
+
+});
+    
+});
+
+
+});
+
+// Filtro por DNI
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    var dni = document.getElementById('buscar')
+    var tabla = document.getElementById('tabla');
+
+rootRef.orderByChild('dni').equalTo(dni.value).on('value', function(snapshot) {
+    tabla.innerHTML ='';   
+snapshot.forEach(function(childSnapshot){
+    var childKey = childSnapshot.key;
+    var childData = childSnapshot.val();
+
+     
+    tabla.innerHTML += `
+    <tr>
+    <th scope="row">${childData.dni}</th>
+    <td>${childData.nombre}</td>
+    <td>${childData.apellido}</td>
+    <td>${childData.email}</td>  
+    <td>${childData.telefono}</td>
+    <td>${childData.edad}</td>
+    <td>${childData.direc}</td>
+    <td>${childData.sexo}</td>
+    <td>${childData.Personal_esencial}</td>
+    <td>${childData.Perdida_gusto_olfato}</td>
+    <td>${childData.Fiebre}</td>
+    <td>${childData.Dolor_de_garganta}</td>
+    <td>${childData.Contacto_estrecho}</td>
+    <td>${childData.Dificultad_respiratoria}</td>
+    <td>${childData.Caso_sospechoso}</td>
+    </tr>
+    `
+
+});
+    
+});
+
+
+});
+
+// Filtro sospechoso
+
+sospechoso.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    var tabla = document.getElementById('tabla');
+
+rootRef.orderByChild('Caso_sospechoso').equalTo('Si').on('value', function(snapshot) {
+    tabla.innerHTML ='';   
+snapshot.forEach(function(childSnapshot){
+    var childKey = childSnapshot.key;
+    var childData = childSnapshot.val();
+
+     
+    tabla.innerHTML += `
+    <tr>
+    <th scope="row">${childData.dni}</th>
+    <td>${childData.nombre}</td>
+    <td>${childData.apellido}</td>
+    <td>${childData.email}</td>  
+    <td>${childData.telefono}</td>
+    <td>${childData.edad}</td>
+    <td>${childData.direc}</td>
+    <td>${childData.sexo}</td>
+    <td>${childData.Personal_esencial}</td>
+    <td>${childData.Perdida_gusto_olfato}</td>
+    <td>${childData.Fiebre}</td>
+    <td>${childData.Dolor_de_garganta}</td>
+    <td>${childData.Contacto_estrecho}</td>
+    <td>${childData.Dificultad_respiratoria}</td>
+    <td>${childData.Caso_sospechoso}</td>
+    </tr>
+    `
+
+});
+    
+});
+
+});
+
+
+// Filtro no sospechoso
+
+noSospechoso.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    var total = 0;
+    var tabla = document.getElementById('tabla');
+
+rootRef.orderByChild('Caso_sospechoso').equalTo('No').on('value', function(snapshot) {
+    tabla.innerHTML ='';   
+
+snapshot.forEach(function(childSnapshot){
+    var childKey = childSnapshot.key;
+    var childData = childSnapshot.val();
+
+    
+
+     
+    tabla.innerHTML += `
+    <tr>
+    <th scope="row">${childData.dni}</th>
+    <td>${childData.nombre}</td>
+    <td>${childData.apellido}</td>
+    <td>${childData.email}</td>  
+    <td>${childData.telefono}</td>
+    <td>${childData.edad}</td>
+    <td>${childData.direc}</td>
+    <td>${childData.sexo}</td>
+    <td>${childData.Personal_esencial}</td>
+    <td>${childData.Perdida_gusto_olfato}</td>
+    <td>${childData.Fiebre}</td>
+    <td>${childData.Dolor_de_garganta}</td>
+    <td>${childData.Contacto_estrecho}</td>
+    <td>${childData.Dificultad_respiratoria}</td>
+    <td>${childData.Caso_sospechoso}</td>
+    </tr>
+    `
+    
+
+});
+
+});
+    
+});
+
+}else{
+    alert("Debe autentificarse antes de acceder a esta página");
+    window.open('http://192.168.0.110:4000/public/login.html', "_self");
+}
+});
