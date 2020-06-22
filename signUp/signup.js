49cf3cfb192a81e5d6f3c7246cfc37b542d979c5
Camilo <camiloagustinjara@gmail.com>
2020-06-22 14:17:34 -0300

    agregue tag al style para poder editar toda la pagina desde ahi, parte del registro es responsive

--- signUp/signup.js
+++ signUp/signup.js
@@ -1,16 +1,25 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyDDRyqvjiteCRv9dtsvepBFntWCj2_2u7A",
-    authDomain: "coronapp-b1f3c.firebaseapp.com",
-    databaseURL: "https://coronapp-b1f3c.firebaseio.com",
-    projectId: "coronapp-b1f3c",
-    storageBucket: "coronapp-b1f3c.appspot.com",
-    messagingSenderId: "971835283337",
-    appId: "1:971835283337:web:0637a51c09501d1970b358",
-    measurementId: "G-HM80TBP5GQ"
-};
-firebase.initializeApp(firebaseConfig);
+const regFormu = document.querySelector('#regform');
+regFormu.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = regFormu['email'].value;
+    const dni = regFormu['dni'].value;
+    const nombre = regFormu['nombre'].value;
+    const apellido = regFormu['apellido'].value;
+    const direc = regFormu['direc'].value;
+    const edad = regFormu['edad'].value;
+
+    var data = { nombre, apellido, email, dni, direc, edad }
+    ref.child(dni).update(data);
+
+    firebase.auth().createUserWithEmailAndPassword(email, dni).then(cred => {
+        console.log(cred.user);
+    });
+})
 
 
+
+/*
 const regFormu = document.querySelector('#regform');
 //Constantes
 const email = regFormu['email'].value;
@@ -37,6 +46,10 @@ regFormu.addEventListener("submit", (e) => {
     document.getElementById("regform").reset();
 });
 
+*/
+
+/*
 function getId(id) {
     return document.getElementById(id).value;
 }
+*/
